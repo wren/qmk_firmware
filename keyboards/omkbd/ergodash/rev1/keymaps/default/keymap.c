27dc19a624aa7a74615867df3a74291dae481854
Jonathan Wren <jonathan@nowandwren.com>
2022-04-14 16:25:31 -0700

    update ergodash keymap

--- keyboards/omkbd/ergodash/rev1/keymaps/default/keymap.c
+++ keyboards/omkbd/ergodash/rev1/keymaps/default/keymap.c
@@ -19,9 +19,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    * | ` ~  |   1  |   2  |   3  |   4  |   5  |   6  ||||||||||||||||||||||   6  |   7  |   8  |   9  |   0  | - _  | = +  |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Tab  |   Q  |   W  |   E  |   R  |   T  |      |||||||||||||||||||||| PgUp |   Y  |   U  |   I  |   O  |   P  | \ |  |
+   * | Tab  |   Q  |   W  |   E  |   R  |   T  | PgUp |||||||||||||||||||||| PgUp |   Y  |   U  |   I  |   O  |   P  | \ |  |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | F-Esc|   A  |   S  |   D  |   F  |   G  |      |||||||||||||||||||||| PgDn |   H  |   J  |   K  |   L  | ; :  | ' "  |
+   * | F-Esc|   A  |   S  |   D  |   F  |   G  | PgDn |||||||||||||||||||||| PgDn |   H  |   J  |   K  |   L  | ; :  | ' "  |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  | Meh  |      ||||||||      | Meh  |   N  |   M  | , <  | . >  | / ?  | Shift|
    * |-------------+------+------+------+------+------+  ENT +------+  ENT +------+------+------+------+------+-------------|
@@ -30,8 +30,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_QWERTY] = LAYOUT(
     KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6   ,                        KC_6   , KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
-    KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX,                        KC_PGUP, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-    FANCY_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    XXXXXXX,                        KC_PGDN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_PGUP,                        KC_PGUP, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+    FANCY_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_PGDN,                        KC_PGDN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MEH ,                        KC_MEH , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
     SYMBOLS,   NUMPAD,  KC_LALT, KC_LCTL,          KC_BSPC, KC_RGUI,  KC_ENT,      KC_ENT,  KC_RGUI, KC_SPC,           KC_RCTL, KC_RALT, NUMPAD,  SYMBOLS
   ),
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+-----------------------------+------+------+------+------+------+------+------|
    * | Shift|      |      |      |      |      | Meh  |       ||||||||       | Meh  |      |      |   <  |   >  |   ?  | Shift|
    * |-------------+------+------+------+------+------+ Enter |||||||| Enter +------+------+------+------+------+-------------|
-   * |SYMBOL|NUMPAD|  Alt | Ctrl |||||||| Space| Cmd  |       ||||||||       | Cmd  | Space|||||||| Ctrl | Alt  |NUMPAD|SYMBOL|
+   * |SYMBOL|NUMPAD|  Alt | Ctrl |||||||| Del  | Cmd  |       ||||||||       | Cmd  | Space|||||||| Ctrl | Alt  |NUMPAD|SYMBOL|
    * ,------------------------------------------------------------------------------------------------------------------------.
    */
   [_SYMBOLS] = LAYOUT(
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH, KC_DLR,  KC_PERC, KC_CIRC,                      KC_CIRC,  KC_AMPR, KC_ASTR, KC_LCBR, KC_RCBR,  XXXXXXX,  KC_PIPE,
     QWERTY,   XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,  XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC,  KC_COLN,  KC_DQUO,
     _______,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, _______,                      _______,  XXXXXXX, XXXXXXX, KC_LT,   KC_GT,    KC_QUES,  _______,
-    _______,  _______,  _______,  _______,          KC_SPC ,  _______,  _______,  _______, _______,  _______,          _______, _______,  _______,  _______
+    _______,  _______,  _______,  _______,          KC_DEL ,  _______,  _______,  _______, _______,  _______,          _______, _______,  _______,  _______
   ),
 
   /* Control
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+-----------------------------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |      | Alt  |       ||||||||       | Alt  |  *   |  1   |  2   |  3   |      |      |
    * |-------------+------+------+------+------+------+ Enter |||||||| Enter +------+------+------+------+------+-------------|
-   * |SYMBOL|NUMPAD|  Alt | Ctrl |||||||| Bspc | Cmd  |       ||||||||       | Space|  0   ||||||||  .   |  .   |      |      |
+   * |SYMBOL|NUMPAD|  Alt | Ctrl |||||||| Space| Cmd  |       ||||||||       | Space|  0   ||||||||  .   |  .   |      |      |
    * ,------------------------------------------------------------------------------------------------------------------------.
    */
   [_NUMPAD] = LAYOUT(
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, KC_PMNS, KC_P7,   KC_P8,   KC_P9,   XXXXXXX, XXXXXXX,
     QWERTY , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,                       _______, KC_PPLS, KC_P4,   KC_P5,   KC_P6,   XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,                       _______, KC_PAST, KC_P1,   KC_P2,   KC_P3,   XXXXXXX, XXXXXXX,
-    _______, _______, _______, _______,          _______, _______,  _______,   _______,  KC_SPC , KC_P0  ,          KC_PDOT, KC_PDOT, XXXXXXX, XXXXXXX
+    _______, _______, _______, _______,          KC_SPC , _______,  _______,   _______,  KC_SPC , KC_P0  ,          KC_PDOT, KC_PDOT, XXXXXXX, XXXXXXX
   )
 };
 
