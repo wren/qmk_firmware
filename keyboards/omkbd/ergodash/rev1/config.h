d1f3c4578bc3fb16e4cd00289a3c57ef3dfa1d58
Jonathan Wren <jonathan@nowandwren.com>
2022-04-30 10:35:48 -0700

    fix some issues around tapping double-function keys

--- keyboards/omkbd/ergodash/rev1/config.h
+++ keyboards/omkbd/ergodash/rev1/config.h
@@ -40,7 +40,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DIODE_DIRECTION COL2ROW
 
 /* define tapping term */
-#define TAPPING_TERM 120
+#define TAPPING_TERM 150
+#define PERMISSIVE_HOLD
 
 /* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
